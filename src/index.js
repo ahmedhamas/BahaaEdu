15801d1b47ae3b6ccaa85c21168abca4d899c458
ahmedhamas <46579089+ahmedhamas@users.noreply.github.com>
2024-11-09 14:44:37 +0200

    Update index.js

--- src/index.js
+++ src/index.js
@@ -15,7 +15,7 @@ app.use(cookieParser());
 
 app.use(
   cors({
-    origin: ["http://localhost:5173"], // Replace with your frontend URL(s)
+    origin: ["*"],
     methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
     preflightContinue: false,
     optionsSuccessStatus: 204,
